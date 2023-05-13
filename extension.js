63ee7a15c9c66a8892496c7e2aba8ce73a21e70b
Jan Ouwens <gnome-happy-appy-hotkey@jqno.nl>
2023-05-13 22:36:33 +0200

    Initial commit

---
+++ extension.js
@@ -0,0 +1,20 @@
+class Extension {
+
+    constructor() {
+    }
+
+    enable() {
+        this.initSettings();
+    }
+
+    disable() {
+    }
+
+    initSettings() {
+    }
+}
+
+function init() {
+    return new Extension();
+}
+
---
+++ prefs.js
@@ -0,0 +1,5 @@
+
+function init() { }
+
+function fillPreferencesWindow(win) {
+}
